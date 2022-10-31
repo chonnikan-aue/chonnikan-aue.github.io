b93991fce4cf584a19da6b22a73b3f4816953540
Chonnikan <chonnikan.aue@gmail.com>
2022-10-31 09:21:08 +0700

    add music, img, contact

--- javascript.js
+++ javascript.js
@@ -4,28 +4,37 @@ function change(content) {
     document.getElementById("skill").style.display = "none";
     if (content == "about") {
         document.getElementById("about").style.display = "block";
-        document.getElementById("nav-exp").className = "";
-        document.getElementById("nav-skill").className = "";
-        document.getElementById("nav-about").className = "active";
+        document.getElementById("nav-exp").className = "navi";
+        document.getElementById("nav-skill").className = "navi";
+        document.getElementById("nav-about").className = "navi active";
     } else if (content == "exp") {
         document.getElementById("exp").style.display = "block";
-        document.getElementById("nav-about").className = "";
-        document.getElementById("nav-exp").className = "active";
-        document.getElementById("nav-skill").className = "";
+        document.getElementById("nav-about").className = "navi";
+        document.getElementById("nav-exp").className = "navi active";
+        document.getElementById("nav-skill").className = "navi";
     } else if (content == "skill") {
         document.getElementById("skill").style.display = "block";
-        document.getElementById("nav-about").className = "";
-        document.getElementById("nav-exp").className = "";
-        document.getElementById("nav-skill").className = "active";
+        document.getElementById("nav-about").className = "navi";
+        document.getElementById("nav-exp").className = "navi";
+        document.getElementById("nav-skill").className = "navi active";
     }
-    i = 0;
     document.getElementById("number").innerText = "";
 }
 
-let i = 0;
-
 function love() {
-    i++;
-    //document.getElementById("love").style.display = "block";
-    document.getElementById("number").innerText = i;
+    document.getElementById("number").innerText = "Thanks for loving!";
+    document.getElementById("pop").play();
+}
+
+function link(link) {
+    window.open(link, "_blank");
+}
+
+function playSound() {
+    document.getElementById("music").muted = !document.getElementById("music").muted;
+    if (document.getElementById("musicButton").innerHTML == '<i class="bi bi-volume-mute-fill"></i>') {
+        document.getElementById("musicButton").innerHTML = '<i class="bi bi-volume-down-fill"></i>';
+    } else {
+        document.getElementById("musicButton").innerHTML = '<i class="bi bi-volume-mute-fill"></i>';
+    }
 }
